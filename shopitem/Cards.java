f22846242dba68f84d3268dd385199b256d90271
Pavel Prokopovich <pavel.prokopovich@gmail.com>
2021-11-19 18:33:32 +0300

    add(Cards.java) : Класс для скидочных кард.
    Содердит поля id и процент

---
+++ shopitem/Cards.java
@@ -0,0 +1,24 @@
+
+package shopitem;
+
+
+public class Cards {
+    
+    private int cardId;
+    private int cardPercent;
+
+    public Cards(int cardId, int cardPercent) {
+        this.cardId = cardId;
+        this.cardPercent = cardPercent;
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public int getCardPercent() {
+        return cardPercent;
+    }
+  
+    
+}
